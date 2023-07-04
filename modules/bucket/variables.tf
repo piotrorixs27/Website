43af7dc92faa423407138fe070b8354f6a038cfb
piotrorixs27 <135148069+piotrorixs27@users.noreply.github.com>
2023-07-04 11:32:43 +0200

    Add files via upload

---
+++ modules/bucket/variables.tf
@@ -0,0 +1,21 @@
+variable "name_bucket" {
+  type=string
+}
+variable "index_document" {
+  type=string
+}
+variable "error_document" {
+  
+}
+variable "block_public_acls" {
+  type=bool
+}
+variable "block_public_policy" {
+  type=bool
+}
+variable "ignore_public_acls" {
+  type=bool
+}
+variable "restrict_public_buckets" {
+  type = bool
+}
